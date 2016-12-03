1832321bd3c13e291c0267d6261ad5d72428f4e2
Janaka-Steph <rochestephane@yahoo.fr>
2016-12-03 03:57:06 +0100

    Add saving contract

---
+++ contracts/savingContract.sol
@@ -0,0 +1,34 @@
+/* ce contrat bloque des fonds pour jusqu'à une date ("experiation") donnée exprimée en seconde
+il est nécessaire d'envoyer les fonds au déploiement. Au delà de la date la fonction "unlock"
+entraine la destruction du contrat et retourne les fonds à l'envoyeur initial
+On peut se servir de http://www.ethereum-alarm-clock.com/ pour faire l'appel à "unlock" automatiquement*/
+pragma solidity ^0.4.6;
+
+contract saving {
+  uint public expiration; // timestamp en secondes.
+  uint public balance;
+  address owner;
+
+
+  function saving(uint _expiration) {
+    owner = msg.sender;
+    expiration = _expiration;
+    //balance = this.balance;
+  }
+
+  function getDeadline() constant returns (uint) {
+      return expiration;
+  }
+
+  function getAmount() constant returns (uint) {
+      return balance;
+  }
+
+  modifier islocked() {
+    if (expiration < block.timestamp) _;
+    }
+
+  function unlock() islocked(){
+      selfdestruct(owner);
+  }
+}
